e01432842cdd8343b276fea247f7b691a098ad09
zhaohaihzb <zhaohaihzb@126.com>
2026-06-05 09:48:25 +0800

    feat: PHP client v1.1.53 - sslEnabled config and method_exists optimization

---
+++ PushConsumerBuilder.php
@@ -0,0 +1,286 @@
+<?php
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache\Rocketmq;
+
+/**
+ * PushConsumerBuilder - Fluent builder for PushConsumer.
+ * Referencing Java PushConsumerBuilder.java
+ */
+class PushConsumerBuilder
+{
+    private $endpoints = '';
+    private $consumerGroup = '';
+    private $credentials = null;
+    private $subscriptionExpressions = [];
+    private $messageListener = null;
+    private $maxCacheMessageCount = 4096;
+    private $maxCacheMessageSizeInBytes = 67108864;
+    private $awaitDuration = 30;
+    private $consumptionThreadCount = 1; // no-op in PHP, stored for API parity
+    private $enableFifoConsumeAccelerator = false;
+    private $enableMessageInterceptorFiltering = false;
+    private $namespace = '';
+    private $fifo = false;
+    private $tlsCredentials = null;
+
+    /**
+     * Set client configuration.
+     *
+     * @param ClientConfiguration $config Client configuration
+     * @return $this
+     */
+    public function setClientConfiguration(ClientConfiguration $config): self
+    {
+        $this->endpoints = $config->getEndpoints();
+        $this->credentials = $config->getSessionCredentialsProvider();
+        $this->namespace = $config->getNamespace();
+        if ($config->getTlsCredentials() !== null) {
+            $this->tlsCredentials = $config->getTlsCredentials();
+        }
+        return $this;
+    }
+
+    /**
+     * Set FIFO mode.
+     *
+     * @param bool $fifo
+     * @return $this
+     */
+    public function setFifo(bool $fifo): self
+    {
+        $this->fifo = $fifo;
+        return $this;
+    }
+    /**
+     * Set the consumer group.
+     *
+     * @param string $consumerGroup Consumer group name
+     * @return $this
+     */
+    public function setConsumerGroup(string $consumerGroup): self
+    {
+        $this->consumerGroup = $consumerGroup;
+        return $this;
+    }
+
+    /**
+     * Set subscription expressions (topic => filter expression).
+     *
+     * @param array $expressions ['topic' => 'expression', ...]
+     * @return $this
+     */
+    public function setSubscriptionExpressions(array $expressions): self
+    {
+        $this->subscriptionExpressions = $expressions;
+        return $this;
+    }
+
+    /**
+     * Register a subscription for a topic.
+     *
+     * @param string $topic
+     * @param string $expression Filter expression (default "*")
+     * @return $this
+     */
+    public function subscribe(string $topic, string $expression = '*'): self
+    {
+        $this->subscriptionExpressions[$topic] = $expression;
+        return $this;
+    }
+
+    /**
+     * Set the message listener callback.
+     *
+     * @param callable $listener function($messageView): int
+     * @return $this
+     */
+    public function setMessageListener(callable $listener): self
+    {
+        $this->messageListener = $listener;
+        return $this;
+    }
+
+    /**
+     * Set max cached message count.
+     *
+     * @param int $count Max cached message count
+     * @return $this
+     * @throws \InvalidArgumentException if count <= 0
+     */
+    public function setMaxCacheMessageCount(int $count): self
+    {
+        if ($count <= 0) {
+            throw new \InvalidArgumentException("maxCacheMessageCount must be > 0");
+        }
+        $this->maxCacheMessageCount = $count;
+        return $this;
+    }
+
+    /**
+     * Set max cached message size in bytes.
+     *
+     * @param int $bytes Max cached message size in bytes
+     * @return $this
+     * @throws \InvalidArgumentException if bytes <= 0
+     */
+    public function setMaxCacheMessageSizeInBytes(int $bytes): self
+    {
+        if ($bytes <= 0) {
+            throw new \InvalidArgumentException("maxCacheMessageSizeInBytes must be > 0");
+        }
+        $this->maxCacheMessageSizeInBytes = $bytes;
+        return $this;
+    }
+
+    /**
+     * Set consumption thread count (no-op in PHP, stored for API parity).
+     *
+     * @param int $count Thread count
+     * @return $this
+     */
+    public function setConsumptionThreadCount(int $count): self
+    {
+        $this->consumptionThreadCount = $count;
+        return $this;
+    }
+
+    /**
+     * Enable FIFO consume accelerator (parallel processing by messageGroup).
+     *
+     * @param bool $enable
+     * @return $this
+     */
+    public function setEnableFifoConsumeAccelerator(bool $enable): self
+    {
+        $this->enableFifoConsumeAccelerator = $enable;
+        return $this;
+    }
+
+    /**
+     * Enable client-side message interceptor filtering.
+     *
+     * @param bool $enable
+     * @return $this
+     */
+    public function setEnableMessageInterceptorFiltering(bool $enable): self
+    {
+        $this->enableMessageInterceptorFiltering = $enable;
+        return $this;
+    }
+
+    /**
+     * Set namespace.
+     *
+     * @param string $namespace Namespace
+     * @return $this
+     */
+    public function setNamespace(string $namespace): self
+    {
+        $this->namespace = $namespace;
+        return $this;
+    }
+
+    /**
+     * Set TLS credentials for the gRPC connection.
+     *
+     * @param TlsCredentials $tlsCredentials
+     * @return $this
+     */
+    public function setTlsCredentials(TlsCredentials $tlsCredentials): self
+    {
+        $this->tlsCredentials = $tlsCredentials;
+        return $this;
+    }
+
+    /**
+     * Build and start the PushConsumer.
+     *
+     * @return PushConsumer
+     * @throws \RuntimeException if required fields not set
+     */
+    public function buildWithoutStart(): PushConsumer
+    {
+        if ($this->endpoints === '') {
+            throw new \RuntimeException("PushConsumer endpoints must be set");
+        }
+        if ($this->consumerGroup === '') {
+            throw new \RuntimeException("PushConsumer consumerGroup must be set");
+        }
+        if ($this->messageListener === null) {
+            throw new \RuntimeException("PushConsumer messageListener must be set");
+        }
+        if (empty($this->subscriptionExpressions)) {
+            throw new \RuntimeException("PushConsumer must have at least one subscription");
+        }
+
+        return new PushConsumer($this->endpoints, $this->consumerGroup, [
+            'subscriptionExpressions' => $this->subscriptionExpressions,
+            'messageListener' => $this->messageListener,
+            'maxCacheMessageCount' => $this->maxCacheMessageCount,
+            'maxCacheMessageSizeInBytes' => $this->maxCacheMessageSizeInBytes,
+            'awaitDuration' => $this->awaitDuration,
+            'fifo' => $this->fifo,
+            'enableFifoConsumeAccelerator' => $this->enableFifoConsumeAccelerator,
+            'namespace' => $this->namespace,
+            'credentials' => $this->credentials,
+            'tlsCredentials' => $this->tlsCredentials,
+        ]);
+    }
+
+    /**
+     * Build and start the PushConsumer synchronously.
+     *
+     * @return PushConsumer
+     * @throws \RuntimeException if required fields not set
+     */
+    public function build(): PushConsumer
+    {
+        $consumer = $this->buildWithoutStart();
+        $consumer->start();
+        return $consumer;
+    }
+
+    /**
+     * Build, start, run for a duration, and shutdown the PushConsumer.
+     *
+     * @param int $seconds Duration in seconds to run
+     * @return void
+     * @throws \RuntimeException if required fields not set
+     */
+    public function startFor(int $seconds):  void
+    {
+        $consumer = $this->buildWithoutStart();
+        $consumer->startWithTimeout($seconds);
+        $consumer->shutdown();
+    }
+
+    /**
+     * Build and start the PushConsumer asynchronously.
+     *
+     * @param callable|null $onDone Optional callback invoked when startup completes
+     * @return PushConsumer
+     * @throws \RuntimeException if required fields not set
+     */
+    public function buildAsync(?callable $onDone = null): PushConsumer
+    {
+        $consumer = $this->buildWithoutStart();
+        $consumer->startAsync($onDone);
+        return $consumer;
+    }
+}
---
+++ SimpleConsumerBuilder.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apache\Rocketmq;
+
+/**
+ * SimpleConsumerBuilder - Fluent builder for SimpleConsumer.
+ * Referencing Java SimpleConsumerBuilder.java
+ */
+class SimpleConsumerBuilder
+{
+    private $endpoints = '';
+    private $consumerGroup = '';
+    private $credentials = null;
+    private $subscriptionExpressions = [];
+    private $awaitDuration = 30;
+    private $namespace = '';
+    private $tlsCredentials = null;
+
+    /**
+     * Set client configuration.
+     *
+     * @param ClientConfiguration $config Client configuration
+     * @return $this
+     */
+    public function setClientConfiguration(ClientConfiguration $config): self
+    {
+        $this->endpoints = $config->getEndpoints();
+        $this->credentials = $config->getSessionCredentialsProvider();
+        $this->namespace = $config->getNamespace();
+        if ($config->getTlsCredentials() !== null) {
+            $this->tlsCredentials = $config->getTlsCredentials();
+        }
+        return $this;
+    }
+
+    /**
+     * Set the consumer group.
+     *
+     * @param string $consumerGroup Consumer group name
+     * @return $this
+     */
+    public function setConsumerGroup(string $consumerGroup): self
+    {
+        $this->consumerGroup = $consumerGroup;
+        return $this;
+    }
+
+    /**
+     * Set subscription expressions.
+     *
+     * @param array $expressions ['topic' => 'expression', ...]
+     * @return $this
+     */
+    public function setSubscriptionExpressions(array $expressions): self
+    {
+        $this->subscriptionExpressions = $expressions;
+        return $this;
+    }
+
+    /**
+     * Subscribe to a topic.
+     *
+     * @param string $topic
+     * @param string $expression Filter expression (default "*")
+     * @return $this
+     */
+    public function subscribe(string $topic, string $expression = '*'): self
+    {
+        $this->subscriptionExpressions[$topic] = $expression;
+        return $this;
+    }
+
+    /**
+     * Set long-polling await duration in seconds.
+     *
+     * @param int $seconds Await duration in seconds
+     * @return $this
+     * @throws \InvalidArgumentException if seconds is not positive
+     */
+    public function setAwaitDuration(int $seconds): self
+    {
+        if ($seconds <= 0) {
+            throw new \InvalidArgumentException("awaitDuration must be > 0");
+        }
+        $this->awaitDuration = $seconds;
+        return $this;
+    }
+
+    /**
+     * Set namespace.
+     *
+     * @param string $namespace Namespace string
+     * @return $this
+     */
+    public function setNamespace(string $namespace): self
+    {
+        $this->namespace = $namespace;
+        return $this;
+    }
+
+    /**
+     * Set TLS credentials for the gRPC connection.
+     *
+     * @param TlsCredentials $tlsCredentials
+     * @return $this
+     */
+    public function setTlsCredentials(TlsCredentials $tlsCredentials): self
+    {
+        $this->tlsCredentials = $tlsCredentials;
+        return $this;
+    }
+
+    /**
+     * Build and start the SimpleConsumer.
+     *
+     * @return SimpleConsumer
+     * @throws \RuntimeException if required fields not set
+     */
+    public function build(): SimpleConsumer
+    {
+        if ($this->endpoints === '') {
+            throw new \RuntimeException("SimpleConsumer endpoints must be set");
+        }
+        if ($this->consumerGroup === '') {
+            throw new \RuntimeException("SimpleConsumer consumerGroup must be set");
+        }
+        if (empty($this->subscriptionExpressions)) {
+            throw new \RuntimeException("SimpleConsumer must have at least one subscription");
+        }
+
+        $consumer = new SimpleConsumer($this->endpoints, $this->consumerGroup, [
+            'subscriptionExpressions' => $this->subscriptionExpressions,
+            'awaitDuration' => $this->awaitDuration,
+            'namespace' => $this->namespace,
+            'credentials' => $this->credentials,
+            'tlsCredentials' => $this->tlsCredentials,
+        ]);
+
+        $consumer->start();
+        return $consumer;
+    }
+}
